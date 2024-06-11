ddce6353c530d2b78e22222849c9f4ebfa2132d2
sandaliademercado <38840503+seisporseis@users.noreply.github.com>
2024-06-11 20:17:52 +0200

    update commit

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+import { src, dest, watch } from 'gulp'
+import * as dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+
+const sass = gulpSass(dartSass)
+
+export function css( done ) {
+    src('src/scss/app.scss', {sourcemaps: true})
+        .pipe( sass().on('error', sass.logError) )
+        .pipe( dest('build/css', {sourcemaps: '.'}) )
+
+    done()
+}
+
+export function dev() {
+    watch('src/scss/**/*.scss', css)
+}
