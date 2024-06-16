c81dc76e75bab8deb021de7fc13d7949517f9e46
sandaliademercado <38840503+seisporseis@users.noreply.github.com>
2024-06-16 20:36:52 +0200

    last update commit

--- build/js/app.js
+++ build/js/app.js
@@ -1,6 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
+    fixedNav()
     createGallery()
+    highlightedLink()
+    scrollNav()
 })
+
+function fixedNav() {
+    const header = document.querySelector('.header')
+    const aboutFestival = document.querySelector('.about-festival')
+
+    window.addEventListener('scroll', function() {
+        if (aboutFestival.getBoundingClientRect().bottom < 1) {
+            header.classList.add('fixed')
+        } else {
+            header.classList.remove('fixed')
+        }
+    })
+}
 
 function createGallery() {
     const CANTIDAD_IMAGENES = 16
@@ -10,6 +26,88 @@ function createGallery() {
         imagen.src = `src/img/gallery/full/${i}.jpg`
         imagen.alt = 'Imagenes de la galería'
 
+        //Event handler
+        imagen.onclick = function() {
+            showImage(i)
+        }
+
         gallery.appendChild(imagen)
     }
+}
+
+function showImage(i) {
+    const imagen = document.createElement('IMG')
+    imagen.src = `src/img/gallery/full/${i}.jpg`
+    imagen.alt = 'Imagenes de la galería'
+
+    //Crear modal
+    const modal = document.createElement('DIV')
+    modal.classList.add('modal')
+    modal.onclick = closeModal
+
+    //Botón cerrar modal
+    const closeModalBtn = document.createElement('BUTTON')
+    closeModalBtn.textContent = 'X'
+    closeModalBtn.classList.add('btn-close')
+    closeModalBtn.onclick = closeModal
+
+    modal.appendChild(imagen)
+    modal.appendChild(closeModalBtn)
+
+    //Agregar al HTML
+    const body = document.querySelector('body')
+    body.classList.add('overflow-hidden')
+    body.appendChild(modal)
+}
+
+function closeModal() {
+    const modal = document.querySelector('.modal')
+    modal.classList.add('fade-out')
+
+    setTimeout(() => {
+            modal?.remove()
+
+            const body = document.querySelector('body')
+            body.classList.remove('overflow-hidden')
+
+    }, 500);
+}
+
+function highlightedLink() {
+    document.addEventListener('scroll', function() {
+        const sections = document.querySelectorAll('section')
+        const navLinks = document.querySelectorAll('.principal-menu a')
+
+        let actual = '';
+        sections.forEach( section => {
+            const sectionTop = section.offsetTop
+            const sectionHeight = section.clientHeight
+
+            if(window.scrollY >= (sectionTop - sectionHeight / 3) ) {
+                actual = section.id
+            }
+        })
+
+        navLinks.forEach( link => {
+            link.classList.remove('active')
+            if(link.getAttribute('href') === '#' + actual) {
+                link.classList.add('active')
+            }
+        })
+    })
+}
+
+function scrollNav() {
+    const navLinks = document.querySelectorAll('.principal-menu a')
+
+     navLinks.forEach( link => {
+        link.addEventListener('click', e => {
+            e.preventDefault()
+            const sectionScroll = e.target.getAttribute('href')
+            const section = document.querySelector(sectionScroll)
+
+            section.scrollIntoView({behavior: 'smooth'})
+        })
+     }
+     )
 }
